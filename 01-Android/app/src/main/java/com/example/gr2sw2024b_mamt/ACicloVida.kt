2ab5af25892d09347a39d83bc07fe613b5fc4bab
Matías Mejía Terán <matias.mejia@epn.edu.ec>
2024-12-05 10:41:39 -0500

    Clase 11 - Ciclo de vida de una actividad

---
+++ 01-Android/app/src/main/java/com/example/gr2sw2024b_mamt/ACicloVida.kt
@@ -0,0 +1,83 @@
+package com.example.gr2sw2024b_mamt
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
+class ACicloVida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_aciclo_vida)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_ciclo_vida)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        mostrarSnackbar("OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackbar("OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("OnResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        mostrarSnackbar("OnRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar("OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar("OnDestroy")
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle){
+        outState.run {
+            // Guardamos las variables
+            putString("variableTextoGuardado", textoGlobal)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        //recuperamos las variables, el texto recuperado
+        val textRecuperado: String? = savedInstanceState.getString("variableTextoGuardado")
+        if (textRecuperado != null){
+            mostrarSnackbar(textRecuperado)
+        }
+    }
+
+    var textoGlobal = ""
+    fun mostrarSnackbar(texto: String){
+        textoGlobal += texto
+
+        var snack = Snackbar.make(
+            findViewById(R.id.cl_ciclo_vida),
+            textoGlobal,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack. show()
+
+    }
+}
--- 01-Android/app/src/main/java/com/example/gr2sw2024b_mamt/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/gr2sw2024b_mamt/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gr2sw2024b_mamt
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +12,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_main) //le digo a la actividad cual layout va a utilizar
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_ciclo_vida)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida.setOnClickListener {
+            irActividad(ACicloVida::class.java)
+        }
+
+    }
+
+    fun irActividad(clase: Class<*>){
+        startActivity(Intent(this, clase))
     }
 }
